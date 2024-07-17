a8442cb9b661d96d627dc8134b7ec7dc2ce69618
NoushinP <noushinazari89@gmail.com>
2024-07-17 18:18:31 -0500

    features-seeds

---
+++ seeds/index.js
@@ -0,0 +1,16 @@
+const sequelize = require('../config/connection')
+const { User, Favorites } = require('../models')
+const userSeedData = require('./user.json')
+const favoritesSeedData = require('./favorites.json')
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    await User.bulkCreate(userSeedData, {
+        returning: true
+    })
+    await Favorites.bulkCreate(favoritesSeedData, {
+        returning: true
+    })
+    process.exit(0);
+};
+seedDatabase();
